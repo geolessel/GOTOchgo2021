499c561cf6a8235577931cfdf555e39de4495452
Geoffrey Lessel <geo@geoffreylessel.com>
2021-04-08 20:51:01 -0700

    Add price updating background timer

--- lib/gotochgo.ex
+++ lib/gotochgo.ex
@@ -8,9 +8,35 @@ defmodule Gotochgo do
   """
 
   alias Gotochgo.FakeRepo, as: Repo
+  alias Gotochgo.Company
+
+  @max_movement_percent_tenths 1
+  @increase_price_change_percent 60
 
   @spec list_companies :: list({ticker :: String.t(), name :: String.t()})
   def list_companies do
     Repo.all(:companies)
+  end
+
+  @spec update_price(Company.t()) :: Company.t()
+  def update_price(%Company{price: price} = company) do
+    price_change = price * (:rand.uniform(@max_movement_percent_tenths * 100) / 100_000)
+
+    new_price =
+      case :rand.uniform(100) do
+        chance when chance in 0..@increase_price_change_percent ->
+          price + price_change
+
+        _ ->
+          price - price_change
+      end
+
+    Map.put(company, :price, new_price)
+  end
+
+  @spec update_all_prices([Company.t()]) :: [Company.t()]
+  def update_all_prices(companies) do
+    companies
+    |> Enum.map(&update_price/1)
   end
 end
--- lib/gotochgo/fake_repo.ex
+++ lib/gotochgo/fake_repo.ex
@@ -17,13 +17,18 @@ defmodule Gotochgo.FakeRepo do
     GenServer.call(__MODULE__, :inspect)
   end
 
+  def restart_timer do
+    GenServer.cast(__MODULE__, :restart_timer)
+  end
+
   # ┌──────────────────┐
   # │ Server Callbacks │
   # └──────────────────┘
 
   def init(_state) do
     companies = Gotochgo.SAndP500.companies()
-    {:ok, %{companies: companies}}
+    {:ok, timer} = build_timer()
+    {:ok, %{companies: companies, timer: timer}}
   end
 
   def handle_call({:all, key}, _, state) do
@@ -33,4 +38,19 @@ defmodule Gotochgo.FakeRepo do
   def handle_call(:inspect, _, state) do
     {:reply, state, state}
   end
+
+  def handle_cast(:restart_timer, state) do
+    with {:ok, _} <- :timer.cancel(state.timer),
+         {:ok, timer} <- build_timer() do
+      {:noreply, Map.put(state, :timer, timer)}
+    end
+  end
+
+  def handle_info(:tick, state) do
+    companies = Gotochgo.update_all_prices(state.companies)
+    {:noreply, Map.put(state, :companies, companies)}
+  end
+
+  def update_ms, do: 2_000
+  def build_timer, do: :timer.send_interval(update_ms(), :tick)
 end

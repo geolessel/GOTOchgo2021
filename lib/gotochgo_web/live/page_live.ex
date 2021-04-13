221f79250665d479e30b6fd0332b5026494bf5fe
Geoffrey Lessel <geo@geoffreylessel.com>
2021-04-12 18:14:57 -0700

    Move getting list of companies to new `mount` function

--- lib/gotochgo_web/live/page_live.ex
+++ lib/gotochgo_web/live/page_live.ex
@@ -1,8 +1,12 @@
 defmodule GotochgoWeb.PageLive do
   use Phoenix.LiveView
 
-  def render(_assigns) do
+  def mount(_params, _session, socket) do
     companies = Gotochgo.list_companies()
-    GotochgoWeb.PageView.render("index.html", companies: companies)
+    {:ok, assign(socket, :companies, companies)}
+  end
+
+  def render(assigns) do
+    GotochgoWeb.PageView.render("index.html", assigns)
   end
 end
